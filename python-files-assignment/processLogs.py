30615a659e25ef18078055d758945123785dbd25
apurva <pathakapurv0@gmail.com>
2026-02-23 05:42:45 +0530

    output done with logs

--- python-files-assignment/processLogs.py
+++ python-files-assignment/processLogs.py
@@ -2,9 +2,16 @@ nos = []
 with open("numbers.txt", "r") as file:
     for line in file:
         nos.append(int(line.strip()))
+n = len(nos)
+total = sum(nos)
+avg = sum(nos)/len(nos)
+print(f"Total Numbers: {n}")
+print(f"Total Sum: {total}")
+print(f"Average : {avg}")
 
-print(f"Total Numbers: {len(nos)}")
-print(f"Total Sum: {sum(nos)}")
-print(f"Average : {sum(nos)/len(nos)}")
-
-#with open("results.log")
+with open("results.log", "a") as file:
+    file.write("File opened successfully")
+    file.write("\nData loaded : " + " ".join(f"{nos[i]}" for i in range(n)))
+    file.write(f"\nTotal numbers: {total}")
+    file.write(f"\nSum: {total} \nAverage = {avg}")
+    file.write("\nComputation completed")
