569266ea4848a485637930fb52247361a7c5a070
apurva <pathakapurv0@gmail.com>
2026-03-03 04:48:33 +0530

    loaded/analyzed

--- python-pdIntro-assignment/practice.py
+++ python-pdIntro-assignment/practice.py
@@ -1,2 +1,15 @@
 import pandas as pd
 
+#loading data
+df = pd.read_csv("archive/data.csv")
+
+#first and last 5 entries
+print("first 5 rows: \n", df.head(5))
+print("\nlast 5 rows: \n",df.tail(5),"\n")
+
+#printing data info
+df.info()
+
+#data summary
+print("\nsummary statistics of numerical columns: \n",df.describe())
+
